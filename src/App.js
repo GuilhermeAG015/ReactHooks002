d1607b03b623862680dc1581575f44f6c00b51f8
Guilherme Grilo <guilhermegrilo2009@hotmail.com>
2021-10-06 15:28:44 -0300

    :sparkles: Creating a base forms

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
+import React from "react";
+import Forms from "./components/Forms/Forms";
+import {Container, Typography } from "@material-ui/core"
+import 'fontsource-roboto';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Init</h1>      
-    </div>
-  );
+    <Container component="article" maxWidth="sm">
+      <Typography variant="h3" component="h1" align="center" >Formulário de cadastro</Typography>
+      <Forms OnSubmitForm={OnSubmitForm} CpfIsValid={CpfIsValid} />
+    </Container>
+    );
+}
+
+function OnSubmitForm(dados){
+  console.log(dados);
+}
+
+function CpfIsValid(cpf){
+  if(cpf.length !== 11){
+    return {valido:false, texto:"CPF deve ter 11 digitos."}
+  }else{
+    return {valido:true, texto:""}
+  }
 }
 
 export default App;
